f6fc02990e6c164b8f0ee2b071785855e34f71c0
MonsterHienDX <hiendx@monsterstudio.io>
2022-02-25 15:35:15 +0700

    delete somethings

--- Assets/Scripts/Item/SO_ItemList.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "so_ItemList", menuName = "Scriptable Objects/Item/Item List")]
-public class SO_ItemList : ScriptableObject
-{
-    [SerializeField]
-    public List<ItemDetails> itemDetails;
-
-}
